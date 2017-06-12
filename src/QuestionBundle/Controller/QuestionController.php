800ab949762f7cb40e75422a0d44fa6ea5308ef0
jeremyBaron <baron.jeremy@orange.fr>
2017-06-13 00:24:19 +0200

    Regular questions implementation

--- src/QuestionBundle/Controller/QuestionController.php
+++ src/QuestionBundle/Controller/QuestionController.php
@@ -46,7 +46,7 @@ class QuestionController extends Controller
      * @param Request $request
      * @return RedirectResponse|Response
      */
-    public function createAction(Request $request)
+    public function createQcmAction(Request $request)
     {
         // create a question and give it some dummy data for this example
         $question = new Question();
@@ -83,6 +83,51 @@ class QuestionController extends Controller
         }
 
         return $this->render('QuestionBundle:Question:new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    public function createRegularAction(Request $request)
+    {
+        // create a question and give it some dummy data for this example
+        $question = new Question();
+
+        /** @var Form $form */
+        $form = $this->createFormBuilder($question)
+            ->add('title', TextType::class, ['label' => 'Title'])
+            ->add('tag', TextType::class, ['label' => 'Tag', 'required' => false])
+            ->add('suggestions', CollectionType::class, array(
+                'label' => false,
+                'entry_type'   => SuggestionType::class,
+                'allow_add'    => true,
+                'by_reference' => false,
+            ))
+            ->add('save', SubmitType::class, ['label' => 'Create'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $suggestion = $question->getSuggestions();
+            foreach($suggestion as $suggestion){
+                $suggestion->setIsCorrect(true);
+            }
+            // $form->getData() holds the submitted values
+            $user = $this->get('security.token_storage')->getToken()->getUser();
+            /** @var Question $question */
+            $question = $form->getData();
+            $question->setUser($user);
+            $question->setType(2);
+            $question->setCreatedAt(new \DateTime('now', new \DateTimeZone('UTC')));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($question);
+            $em->flush();
+
+            return $this->redirectToRoute('question_view', ['id' => $question->getId()]);
+        }
+
+        return $this->render('QuestionBundle:Question:newRegular.html.twig', [
             'form' => $form->createView()
         ]);
     }
